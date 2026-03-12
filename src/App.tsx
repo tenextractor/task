e8696ae3ac3d3a71593008682f08ca31967ccbe0
tenextractor <139619642+tenextractor@users.noreply.github.com>
2026-03-12 15:56:04 +0530

    finished task

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    }
+  }
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
+}
+
+async function getUsers(): Promise<User[]> {
+  const URL = "https://jsonplaceholder.typicode.com/users"
+  const response = await fetch(URL);
+
+  if (!response.ok) {
+    throw new Error(`fetch ${URL} failed with status: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+type FetchState = "loading" | "success" | "error" | "nodata";
+
+function App() {
+  const [fetchState, setFetchState] = useState<FetchState>("loading");
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      try {
+        const fetchedUsers = await getUsers();
+        if (fetchedUsers.length === 0) {
+          setFetchState("nodata");
+          return;
+        }
+        setUsers(fetchedUsers);
+        setFetchState("success");
+      } catch (error) {
+        console.log(error);
+        setFetchState("error");
+      }
+    }
+
+    loadUsers();
+  }, [])
+
+  switch (fetchState) {
+    case "loading":
+      return <div>Loading...</div>
+    case "error":
+      return <div>An error has occurred!</div>
+    case "nodata":
+      return <div>The backend has not returned any data.</div>
+    case "success":
+      return (
+        <table className="border-separate border-spacing-x-8">
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+            </tr>
+          ))}
+        </table>
+      )
+  }
+}
+
+export default App
